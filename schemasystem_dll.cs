e09da0d5694049db58b2c6fcef1ff9dd4aef6a86
Reloading :p <coledonaldf67@gmail.com>
2026-04-21 04:25:37 +0100

    Add files via upload

--- schemasystem_dll.cs
+++ schemasystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-04-19 17:15:56.019092900 UTC
+// 2026-04-21 03:23:19.397130200 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: schemasystem.dll
